689ad179bcc49c9d10cf65079ac4b5a039b067d5
MichaelBourloo <michael.bourloo@gmail.com>
2018-06-05 16:10:35 +0200

    rd1.0

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -4,6 +4,24 @@ var hi = anime ({
     duration: 3000,
 });
 
+// // Fonction exécutée au redimensionnement
+// function animation() {
+//     var hi = document.getElementById('hi');
+//     if("matchMedia" in window) { // Détection
+//       if(window.matchMedia("(min-width:600px)").matches) {
+//         targets: '#hi',
+//         translateX: 280,
+//         duration: 3000,
+//       } else {
+//         targets: '#hi',
+//         translateX: 60,
+//         duration: 3000,
+//       }
+//     }
+//   }
+//   // On lie l'événement resize à la fonction
+//   window.addEventListener('resize', animation, false);
+
 var spada = anime ({
     targets: '#spada',
     translateX: 170,
