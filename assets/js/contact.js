3bd26a3a971586c8fcfbc5c1d12a7c27027ec662
MichaelBourloo <michael.bourloo@gmail.com>
2018-06-05 10:41:50 +0200

    new function scroll

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,11 +1,33 @@
 let button = document.getElementById('btn5');
 let modal = document.getElementById('modal');
 
-button.addEventListener('click', function() {
-    modal.classList.toggle('show'); 
-    button.scrollTop = button.scrollHeight;   
-});
+const smoothScrollTo = (endY, duration = 400) => {
+    const startY = window.pageYOffset,
+    distanceY = endY - startY,
+    startTime = new Date().getTime();
+    
+    const easeInOut = (time, from, distance, duration) => {
+        if ((time /= duration / 2) < 1) {
+            return distance / 2 * time * time * time * time + from;
+        } else {
+        return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
+    }
+    }
+    
+    const timer = setInterval(() => {
+        const time = new Date().getTime() - startTime,
+      newY = easeInOut(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+    }
+    scrollTo(0, newY);
+    }, 1000/60);
+}
 
+button.addEventListener('click', function() {
+    modal.classList.toggle('show');
+    smoothScrollTo((window.innerHeight * 1.25), 1000);     
+});
 
 
 
